1ef40fc333944bd8b936f11b964a09477c915acf
Ubuntu 64-bit local drive <irfan.hamid@gmail.com>
2017-06-04 01:57:15 -0700

    Improve palindrome. Fix readme format.

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 This is a problem solving repository. Mostly to prepare for interviews. Each directory will contain one problem and its solution.
 
-euler
-  |---> shared: basic data structure definitions
+ euler
 
-  |---> euler: the solutions to problems from the http://eulerproject.net
+   |---> shared: basic data structure definitions
 
-          |---> *
+   |---> euler: the solutions to problems from the http://eulerproject.net
 
-  |---> trees: problems based on trees, traversal, searches, etc.
+   |---> trees: problems based on trees, traversal, searches, etc.
 
           |---> traversals: iterative implementations of the inorder, preorder and postorder traversals
 
@@ -15,8 +14,8 @@ euler
 
           |---> reconstruct: rebuild a tree from its inorder and preorder traversals
 
-  |---> strings: problems based on strings
-
+   |---> strings: problems based on strings
+ 
           |---> reverse: reverse a string in-place, i.e., without using auxiliary storage space
 
           |---> reversewords: reverse the words in a string in-place
@@ -25,13 +24,12 @@ euler
 
           |---> longestpalindrome: find the longest palindrome in a string
 
-  |---> linklist: problems based on link lists
+   |---> linklist: problems based on link lists
 
           |---> arblink: reconstruct a link list which has one next and one arbitrary pointer pointing to any node in the link list
 
-  |---> graphs: graph search algorithms
+   |---> graphs: graph search algorithms
 
           |---> reconstruct: given an adjacency list, reconstruct a graph, even if it has cycles
 
-  |---> esoteric: just what the name says
-
+   |---> esoteric: just what the name says
--- strings/longestpalindrome/longestpalindrome.cpp
+++ strings/longestpalindrome/longestpalindrome.cpp
@@ -7,63 +7,33 @@
 
 using namespace std;
 
-void LongestPalindromesOfStride(const char* str, int stride, int* memo)
+int PalindromeLength(const char* str, int strLen, int i)
 {
-  int strLen = strlen(str);
-  int left = 0;
-  
-  if (stride == 1)
-  {
-    for (int i = 0; i + stride < strLen; ++i)
-      if (str[i] == str[i + 1])
-        memo[i * strLen + 1] = 1;
-  }
-  else
-  {
-    for (int i = 0; i + stride < strLen; i += stride)
-    {
-      if (str[i] == str[i + stride] && memo[(i + 1) * strLen + i + stride - 1])
-        memo[i * strLen + stride] = 1;
+  int j = 1;
+  for (j = 1; i+j < strLen && i-j >= 0; ++j) {
+    if (str[i + j] != str[i - j]) {
+      return j;
     }
   }
+  return j-1;
 }
 
-void LongestPalindrome(const char* str)
+int* Palindromes(const char* str)
 {
   int strLen = strlen(str);
-  if (!(str && strLen))
-    return;
-
-  int* memo = new int[strLen * strLen];
-  memset((void*)memo, 0, sizeof(int) * strLen * strLen);
-  for (int i = 0; i < strLen; ++i)
-    memo[i * strLen + i] = 1;
-
-  for (int i = 1; i < strLen; ++i)
-    LongestPalindromesOfStride(str, i, memo);
-
-  int longest = 1;
-  int left = 0, right = 0;
-  for (int i = 0; i < strLen; ++i)
-  {
-    for (int j = strLen - 1; j > 0; --j)
-    {
-      if (memo[i * strLen + j])
-      {
-        if ((j - i) > longest)
-        {
-          longest = j - i;
-          left = i;
-          right = j;
-        }
-      }
-    }
+  if (!(str && strLen)) {
+    return NULL;
+  }
+  int *ret = new int[strLen];
+  for (int i = 0; i < strLen; ++i) {
+    ret[i] = 0;
   }
 
-  char palindrome[1024];
-  memcpy(palindrome, &str[left], right - left + 1);
-  palindrome[right - left + 1] = '\0';
-  cout << palindrome << endl;
+  for (int i = 1; i < strLen - 1; ++i) {
+    ret[i] = PalindromeLength(str,strLen,  i);
+  }
+
+  return ret;
 }
 
 int main()
@@ -74,7 +44,21 @@ int main()
     cin.getline(buffer, 1024);
     if (!strcmp(buffer, "exit"))
       break;
-    LongestPalindrome(buffer);
+    int *pals = Palindromes(buffer);
+    int longest = 0;
+    int ctr = 0;
+    for (int i = 0; i < strlen(buffer); ++i) {
+      if (pals[i] > longest) {
+        longest = pals[i];
+        ctr = i;
+      }
+    }
+    if (longest > 1) {
+      for (int i = ctr-longest; i <= ctr+longest; ++i) {
+        cout << buffer[i];
+      }
+      cout << endl;
+    }
   }
 
   return 0;
